3c2ded8c941d3f21303f8aea9a431928f4fb2d55
mickey <qq523107430@163.com>
2021-09-04 12:16:33 +0800

    feat(refactor): add logic: shutdown http server gracefully

---
+++ hook/hook.go
@@ -0,0 +1,71 @@
+package hook
+
+// import (
+// 	"context"
+// 	"errors"
+// 	"github/http-server/server"
+// 	"log"
+// 	"os"
+// 	"os/signal"
+// 	"sync"
+// 	"time"
+// )
+
+// type Hook func(context.Context) error
+
+// func BuildHook(servers ...server.Server) Hook {
+// 	return func(c context.Context) error {
+// 		var wg sync.WaitGroup
+// 		allDone := make(chan struct{})
+// 		wg.Add(len(servers))
+// 		for _, s := range servers {
+// 			go func(srv server.Server) {
+// 				defer wg.Done()
+// 				err := srv.Shutdown(ctx)
+// 				if err != nil {
+// 					log.Printf("server shutdown error: %v \n", err)
+// 				}
+// 				time.Sleep(1 * time.Second)
+// 			}(s)
+// 		}
+
+// 		return nil
+
+// 		go func() {
+// 			wg.Wait()
+// 			allDone <- struct{}{}
+// 		}()
+
+// 		select {
+// 		case <-c.Done():
+// 			log.Println("closing servers timeout")
+// 			return errors.New("closing hooks timeout\n")
+// 		case <-allDone:
+// 			log.Println("close all servers\n")
+// 			return nil
+// 		}
+
+// 	}
+// }
+
+// func WaitForShutdown(hooks ...Hook) {
+// 	signalChan := make(chan os.Signal, 1)
+// 	signal.Notify(signalChan, nil)
+// 	select {
+// 	case sig := <-signalChan:
+// 		log.Printf("get signal %s, application will shutdown\n", sig)
+// 		time.AfterFunc(time.Minute*10, func() {
+// 			log.Printf("shutdown gracefully timeout, applicaiton shutdown.\n")
+// 			os.Exit(1)
+// 		})
+// 		for _, h := range hooks {
+// 			ctx, cancel := context.WithTimeout(context.Background(), time.Second*20)
+// 			err := h(ctx)
+// 			if err != nil {
+// 				log.Printf("failed to run hook,err: %v", err)
+// 			}
+// 			cancel()
+// 		}
+// 		os.Exit(0)
+// 	}
+// }
--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 	"github/http-server/sdk"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
 
-	var filters = []filter.Filter{ filter.MetricFilter}
+	var filters = []filter.Filter{filter.MetricFilter}
 	s := sdk.NewHTTPServer("http-server", filters...)
 
 	s.Route(http.MethodPost, "/signup", router.SignUp)
@@ -18,7 +21,31 @@ func main() {
 	s.Route(http.MethodGet, "/hello", router.Hello)
 
 	log.Print("start http server on address 8080\n")
-	if err := s.Start(":8080"); err != nil {
-		log.Fatalf("start server error: %v", err)
+
+	go func() {
+		if err := s.Start(":8080"); err != nil {
+			log.Fatalf("start server error: %v", err)
+		}
+	}()
+
+	WaitForShutdown()
+}
+
+func WaitForShutdown() {
+	signChan := make(chan os.Signal, 1)
+	signal.Notify(signChan)
+
+	for {
+		select {
+		case sig := <-signChan:
+			log.Printf("get signal: %s\n", sig)
+			time.AfterFunc(time.Minute, func() {
+				log.Print("timeout 1 minute, applicaiton shutdown gracefully\n")
+				os.Exit(1)
+			})
+			log.Print("applicaiton shutdown gracefully\n")
+			os.Exit(0)
+		}
+
 	}
 }
